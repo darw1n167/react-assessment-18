eca5fc64c2bfea3a82cd8da8e99696453c76ad33
Darwin Facelo <darwinfacelo@yahoo.com>
2023-02-23 08:26:28 -0800

    finished test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Instructions from './components/Instructions'
 import Comments from './components/Comments'
+import {useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+      const apiCall = async () => {
+          const response = await fetch('https://jsonplaceholder.typicode.com/comments')
+          const data = await response.json()
+          setComments(data)
+          
+      }
+      apiCall()
+      
+  }, )
+// console.log(comments)
   return (
     <div className="container">
       <Instructions />
-      <Comments />
+      <Comments comments={comments}/>
     </div>
   );
 }
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,16 +1,28 @@
-const Comments = () => {
+// import {useEffect, useState} from 'react'
+
+const Comments = (props) => {
     // Insturction:
     // Use endpoint: https://jsonplaceholder.typicode.com/comments to get a list of comments. 
     // display the comment body on the screen
     // add event listeners to each comment body that when clicked will simply console.log the comment id.
 
     // you will need to use the useEffect hook. Do this in async / await syntax.
+        const handleClick = (e) => {
+            console.log(e.target.id)
+        }
+ 
 
-
-    return(
+    
         // Replace this with your code.
-        <h1>Comments component</h1>
-    )
+        return (
+            
+                props.comments.map((comment) => (
+                    <h1 key={comment.id}id={comment.id} onClick={handleClick}>{comment.body}</h1>
+                    
+                ))
+           
+        )
+        
 }
 
 export default Comments
